76010fd004b38c6d28f8b32a7f3e56248889df70
eric <ericalvernhe@orange.fr>
2015-02-22 23:06:03 +0100

    refactor, clean config

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ var _ = require('underscore');
 var services = require('./services');
 app.route('/services/photos/:id')
 	.get(function(req, res) {
-		services.actions.getPhoto(req, res);	
-			//res.send('Get a random book');
+		services.actions.getPhoto(req, res);
+		//res.send('Get a random book');
 	})
 	.post(function(req, res) {
 		services.actions.addPhoto(req, res);
@@ -33,7 +33,6 @@ app.route('/services/photos/')
 var appsDir = path.join(__dirname, '..', "apps");
 
 app.get('/', function(req, res) {
-	//console.log("ask for "  + req.client._httpMessage.req.originalUrl)
 	res.sendFile(appsDir + "/index.html");
 });
 
@@ -82,16 +81,11 @@ app.use("/bower_components/", function(req, res, next) {
 	res.sendFile(appsDir + req.client._httpMessage.req.originalUrl);
 });
 
-
+//this must be done into photo services.
 app.use("/server/img/photos/", function(req, res, next) {
 	try {
-		//console.dir(req.client._httpMessage.req);
 		var fs = require('fs');
 		var path = require('path');
-		//var bebforeName = __dirname;
-		//hiii dirty hack
-		//__dirname = );
-
 		var pathFile = path.join(__dirname, '..', decodeURI(req.client._httpMessage.req.originalUrl));
 		var fileDir = path.dirname(pathFile);
 		var fileBase = path.basename(pathFile);
@@ -124,8 +118,6 @@ app.use("/server/img/photos/", function(req, res, next) {
 		res.setHeader('Content-Type', 'text/plain');
 		res.sendStatus(404);
 	}
-	//hiii dirty hack...
-	//__dirname = bebforeName;
 });
 
 app.use(function(req, res, next) {
